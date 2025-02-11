5a9088fa02d7b7c441f3f818abaac9a82da33a1c
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2025-02-11 16:21:31 +0400

    Done

--- WebApiAfternoon/Controllers/PlayersController.cs
+++ WebApiAfternoon/Controllers/PlayersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAfternoon.Dtos;
 using WebApiAfternoon.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -28,44 +29,79 @@ namespace WebApiAfternoon.Controllers
         };
         // GET: api/<PlayersController> api/players 
         [HttpGet]
-        public ActionResult<IEnumerable<Player>> GetPlayers()
+        public ActionResult<IEnumerable<PlayerDto>> GetPlayers()
         {
-            return Ok(players);
+            var dataToReturn = players.Select(p => new PlayerDto
+            {
+                City=p.City,
+                PlayerName=p.PlayerName,
+                Score=p.Score
+            });
+
+            return Ok(dataToReturn);
         }
 
         // GET api/<PlayersController>/5
         [HttpGet("{id}")]
-        public ActionResult<Player> Get(int id)
+        public ActionResult<PlayerDto> Get(int id)
         {
             var player = players.FirstOrDefault(x => x.Id == id);
             if (player == null)
             {
                 return NotFound();
             }
-            return Ok(player);
+            var dataToReturn = new PlayerDto
+            {
+                PlayerName=player.PlayerName,
+                City=player.City,
+                Score=player.Score
+            };
+            return Ok(dataToReturn);
         }
 
         // POST api/<PlayersController>
         [HttpPost]
-        public ActionResult<Player> Post([FromBody] Player newPlayer)
+        public ActionResult<PlayerAddDto> Post([FromBody] PlayerAddDto player)
         {
+            var newPlayer = new Player();
             newPlayer.Id = players.Any() ? players.Max(x => x.Id) + 1 : 1;
+
+            newPlayer.PlayerName = player.PlayerName;
+            newPlayer.City = player.City;
+
             players.Add(newPlayer);
             //return Ok(newPlayer);// 200 and object
             //return Created();// 201
-            return CreatedAtAction(nameof(Get), new { id = newPlayer.Id }, newPlayer);// 201
+            return CreatedAtAction(nameof(Get), new { id = newPlayer.Id }, player);// 201
         }
 
         // PUT api/<PlayersController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] Player updatedPlayer)
         {
+            var player = players.FirstOrDefault(p => p.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            player.City=updatedPlayer.City;
+            player.Score=updatedPlayer.Score;   
+            player.PlayerName=updatedPlayer.PlayerName;
+
+            return NoContent();
         }
 
         // DELETE api/<PlayersController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            var player=players.FirstOrDefault(x => x.Id == id);
+            if(player == null)
+            {
+                return NotFound();
+            }
+            players.Remove(player);
+            return NoContent();
         }
     }
 }
